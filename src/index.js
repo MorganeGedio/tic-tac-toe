18c8b48b4e7599c6bc8c25666b50f7278bd444c8
Morgane Gedio <morgane.gedio@gmail.com>
2020-04-12 18:39:56 +0200

    map history of moves & and list them

--- src/index.js
+++ src/index.js
@@ -104,6 +104,19 @@ class Game extends React.Component {
     const current = history[history.length -1];
     const winner = calculateWinner(current.squares);
 
+    // map history of moves to React elements representing buttons
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move : 
+        'Go to game start';
+      return (
+        // for each move => list item that contains a button with on click handler
+        <li> 
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      )
+    })
+
     let status; 
     if(winner) {
       status = 'Winner: ' + winner;
@@ -121,7 +134,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
